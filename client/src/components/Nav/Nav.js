a4565c5360afffe2951b95b56fe4e405d76d52e6
gorbasho <37026147+gorbasho@users.noreply.github.com>
2018-09-24 15:46:43 -0500

    nav active link css

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,24 +1,79 @@
-import React from "react";
+import React, { Component } from "react";
+import { NavLink } from 'react-router-dom';
 import "./Nav.css";
 
 
 
-const Nav = props => (
+class Nav extends Component {
+  state = {
+    multiplier: 1
+  }
 
-  <nav className="navbar fixed-top navbar-expand-lg navbar-dark mb-5" id="mainNav">
-    <a className="navbar-brand p-0 ml-5" href="#pageTop" id="navbarBrand"><h1 id="nameHeader" style={{fontWeight: "400"}}><i className="fas fa-angle-double-up" id="homeArrow"></i></h1></a>
-    <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <div className="navbar-nav ml-auto mr-2">
-          <a id="navHome" className="nav-item nav-link active" href="/">Home</a>
-          <a className="nav-item nav-link" href="/about">About</a>
-          <a className="nav-item nav-link" href="/menu">Menu</a>
-          <a className="nav-item nav-link" href="/social">Social</a>
-      </div>
-    </div>
-  </nav>
-);
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
 
+  handleScroll = () => {
+    var height = window.scrollY;
+    console.log(height);
+    this.getMultiplier(height, 50, 300);
+  }
+
+  getMultiplier = (height, rangeMin, rangeMax) => {
+    var multiplier = (rangeMax - height) / (rangeMax - rangeMin);
+    if (height < rangeMin) {
+      multiplier = 1;
+    }
+    if (height > rangeMax) {
+      multiplier = 0;
+    }
+    this.setState({multiplier: multiplier});
+    this.changeNav(multiplier);
+  }
+
+  changeNav = (multiplier) => {
+    var color = Math.floor((multiplier) * 255);
+    var textOpacity = (0.2 * multiplier) + 0.3;
+    // $("#mainNav").css("background", `rgba(24, 24, 24, ${opacity})`);
+    // $("a.nav-link.active").css("color", `rgb(${color}, ${color}, ${color})`);
+    // $("a.nav-link").not(".active").css("color", `rgba(${color}, ${color}, ${color}, ${textOpacity})`);
+    // $("#homeArrow").css("opacity", `${opacity}`);
+    // if (opacity < 0.1) {
+    //   $("a.navbar-brand").css("visibility", "hidden");
+    // } else {
+    //   $("a.navbar-brand").css("visibility", "visible");
+    // }
+  }
+
+  // forceNav(opacity) {
+  //   $("#mainNav").css("background", `rgba(24, 24, 24, ${opacity})`);
+  //   // navForced = true;
+  // }
+
+  render() {
+
+    let navlinkStyle = null;
+    //old style {opacity: `${(0.2 * this.state.multiplier) + 0.3}`}
+
+    let navVisibility = (this.state.multiplier < 0.1) ? "none" : "flex";
+    console.log(navVisibility);
+
+    return (
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark mb-5" id="mainNav" style={{display: navVisibility, background: `rgba(24, 24, 24, ${this.state.multiplier})`}}>
+        <a className="navbar-brand p-0 ml-5" href="#pageTop" id="navbarBrand"><h1 id="nameHeader" style={{fontWeight: "400"}}><i className="fas fa-angle-double-up" id="homeArrow"></i></h1></a>
+        <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-nav ml-auto mr-2">
+              <NavLink exact id="navHome" className="nav-item nav-link" to="/" activeClassName="active">Home</NavLink>
+              <NavLink className="nav-item nav-link" to="/about" style={navlinkStyle}activeClassName="active">About</NavLink>
+              <NavLink className="nav-item nav-link" to="/menu" style={navlinkStyle} activeClassName="active">Menu</NavLink>
+              <NavLink className="nav-item nav-link" to="/social" style={navlinkStyle}activeClassName="active">Social</NavLink>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 export default Nav;
